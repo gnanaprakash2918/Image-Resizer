edeb5830115279b75298a6b879988ad127c44c13
Gnana Prakash M <gnanaprakash2918@gmail.com>
2024-07-02 00:14:05 +0530

    Make app ready

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
+const isMac = process.platform === 'darwin';
+const isDev = process.env.NODE_ENV !== 'production';
+
+// Create a main window
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Image Resizer',
-    width: '500',
+    width: isDev ? '1000' : '500',
     height: '600',
   });
+
+  // Open devtools if in dev env
+  if (isDev) {
+    console.log('hi');
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+// App is ready
 app.whenReady().then(() => {
   createMainWindow();
+
+  // Implement menu
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
