b0c091eb8533bc1327b0a2959fa76a96cd584040
Gnana Prakash M <gnanaprakash2918@gmail.com>
2024-07-02 17:41:23 +0530

    Add Image file checks

---
+++ renderer/js/renderer.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#img-form');
+const img = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const fileName = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
+
+function loadImage(e) {
+  const file = e.target.files[0];
+  if (!isFileImage(file)) {
+    console.log('Please Select an image');
+    return;
+  }
+
+  console.log('Success');
+}
+
+// Make sure file is image
+function isFileImage(file) {
+  const acceptedImages = [
+    'image/gif',
+    'image/png',
+    'image/jpeg',
+    'image/bmp',
+    'image/webp',
+    'image/tiff',
+    'image/svg+xml',
+    'image/x-icon',
+    'image/heic',
+    'image/heif',
+  ];
+  return file && acceptedImages.includes(file['type']);
+}
+
+console.log(img);
+img.addEventListener('change', loadImage);
